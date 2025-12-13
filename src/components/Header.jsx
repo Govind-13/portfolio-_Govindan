7370cb2ceeff6d95e31f4408968dd1e85ea70583
Govind-13 <govindan.ramu93@gmail.com>
2025-12-13 16:16:55 +0530

    Fix logo display in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
     return (
         <header className="header" style={headerStyle}>
             <div className="container">
-                <a href="#" className="logo">Govindan.R<span className="dot">.</span></a>
+                <a href="#" className="logo">Govindan<span className="dot">.R</span></a>
                 <nav className="navbar">
                     <ul className={`nav-links ${mobileActive ? 'active' : ''}`}>
                         <li><a href="#about" onClick={closeMobileMenu}>About</a></li>
@@ -54,4 +54,5 @@ const Header = () => {
 };
 
 export default Header;
+
 
